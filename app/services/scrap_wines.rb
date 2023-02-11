eae4c2f90f0cc839b832221ee2741bca345aac45
christophe126 <90414556+christophe126@users.noreply.github.com>
2023-02-11 02:04:56 +0100

    scrap in progress

--- app/services/scrap_wines.rb
+++ app/services/scrap_wines.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
-# https://www.wineandco.com/search?pageNumber=2&filters[color][5]=5&filters[note][bettane]=bettane&filters[note][decanter]=decanter&filters[note][rvf]=rvf&filters[note][parker]=parker&filters[note][winespec]=winespec
+# https://www.wineandco.com/search?pageNumber=1&filters[color][5]=5&filters[note][bettane]=bettane&filters[note][decanter]=decanter&filters[note][rvf]=rvf&filters[note][parker]=parker&filters[note][winespec]=winespec
 
 require 'open-uri'
 require 'nokogiri'
 require 'uri'
 
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-
+USER_AGENT    = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 URL           = 'https://www.wineandco.com/search?'
 FILTER_PAGE   = 'pageNumber='
 # FILTER_COLOR  = '&filters[color][6]=6&filters[color][10]=10&filters[color][5]=5&filters[color][1]=1'
-FILTER_COLOR  = '&filters[color][5]=5' # filtre Rosé
+# filtre Rosé
+FILTER_COLOR  = '&filters[color][5]=5'
 FILTER_NOTES  = '&filters[note][bettane]=bettane&filters[note][decanter]=decanter&filters[note][rvf]=rvf&filters[note][parker]=parker&filters[note][winespec]=winespec'
-
 def url_document(page_number = 1)
   main_url = "#{URL}#{FILTER_PAGE}#{page_number}#{FILTER_COLOR}#{FILTER_NOTES}"
   Nokogiri::HTML.parse(URI.parse(main_url).open('Accept-Language' => 'en-US', 'User-Agent' => USER_AGENT).read)
@@ -27,31 +26,54 @@ end
 def fetch_wines_urls(page)
   doc = url_document(page)
   wines = doc.search('.product-list-products.c-products__wrapper > div > div > a')
-  wines.map do |wine|
+  results = []
+  wines.each do |wine|
+    price = wine.search('span.price').text.split[0]
     uri = URI.parse(wine.attributes['href'].value)
     uri.scheme = 'https'
     uri.host = 'www.wineandco.com'
     uri.query = nil
     uri.to_s
+    results << [uri, price]
   end
+  results
 end
 
-def scrape_wine_page(url)
-  doc = Nokogiri::HTML.parse(URI.parse(url).open('Accept-Language' => 'en-US', 'User-Agent' => USER_AGENT).read)
+def scrape_wine_page(values)
+  p "-----------------#{values}-----------"
+  url = values[0]
+  p url
 
-  name    = doc.search("h1").text.strip
-  ref_id  = url.split('-')[-4]
-  price   = doc.search(".l-product__infos__price span span").text
-  p price
-  #year = doc.search(".ipc-link.ipc-link--baseAlt.ipc-link--inherit-color.sc-8c396aa2-1").text.to_i
-  #storyline = doc.search(".sc-16ede01-2").text
-  #director = doc.search('.ipc-metadata-list__item:contains("Director") a').first.text
-  #cast = doc.search('.ipc-metadata-list__item:contains("Stars") a.ipc-metadata-list-item__list-content-item').map { |element| element.text }.uniq
+  doc = Nokogiri::HTML.parse(URI.parse(url).open("Accept-Language" => "en-US", "User-Agent" => USER_AGENT).read)
 
-  {
-    ref_id: ref_id,
-    name: name,
-    price: price
+  # if Expert review
+  unless doc.search('c-product-notations__experts__wrapper').nil?
 
-  }
+    notes_avis_expert = []
+    doc.search('.c-product-notations__experts__item').each do |element|
+      notes_avis_expert << element.search('.c-product-notation-expert__note').text
+    end
+
+    # Wine Description
+    wine_description = []
+    doc.search('.l-product__description .c-product-description p').each do |element|
+      wine_description << element.text
+    end
+
+    # Output Hash for wine
+    {
+      ref_id: url.split('-')[-4].to_i,
+      name: doc.search('h1').text.strip,
+      price: values[1],
+      available: doc.search('.stock').last.text,
+      domaine: doc.search('.c-product-characteristics__text')[0].text,
+      country: doc.search('.c-product-characteristics__text')[1].text,
+      location: doc.search('.c-product-characteristics__text')[2].text,
+      color: doc.search('.c-product-characteristics__text')[3].text,
+      year: doc.search('.c-product-characteristics__text')[4].text,
+      appellation: doc.search('.c-product-characteristics__text')[5].text,
+      wine_description: wine_description,
+      notes_avis_expert: notes_avis_expert
+    }
+  end
 end
